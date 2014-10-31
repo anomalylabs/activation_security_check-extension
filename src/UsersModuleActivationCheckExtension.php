b671a00327aacc4d3d824e7c2bb0f80dcb06300f
Ryan Thompson <service@aiwebsystems.com>
2014-10-30 21:10:12 -0500

    Yay

---
+++ src/UsersModuleActivationCheckExtension.php
@@ -0,0 +1,76 @@
+<?php namespace Anomaly\Streams\Addon\Extension\UsersModuleActivationCheck;
+
+use Anomaly\Streams\Addon\Module\Users\Activation\Contract\ActivationInterface;
+use Anomaly\Streams\Addon\Module\Users\Activation\Contract\ActivationRepositoryInterface;
+use Anomaly\Streams\Addon\Module\Users\Activation\Exception\UserNotActivatedException;
+use Anomaly\Streams\Addon\Module\Users\Extension\SecurityCheckInterface;
+use Anomaly\Streams\Addon\Module\Users\User\Contract\UserInterface;
+use Anomaly\Streams\Platform\Addon\Extension\ExtensionAddon;
+
+/**
+ * Class UsersModuleActivationCheckExtension
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Addon\Extension\UsersModuleActivationCheckExtension
+ */
+class UsersModuleActivationCheckExtension extends ExtensionAddon implements SecurityCheckInterface
+{
+
+    /**
+     * The activation repository interface object.
+     *
+     * @var
+     */
+    protected $activations;
+
+    /**
+     * Create a new UsersModuleActivationCheckExtension instance.
+     *
+     * @param ActivationRepositoryInterface $activations
+     */
+    /*function __construct(ActivationRepositoryInterface $activations)
+    {
+        $this->activations = $activations;
+    }*/
+
+    /**
+     * Security check during login.
+     *
+     * @param UserInterface $user
+     * @return mixed
+     */
+    public function login(UserInterface $user)
+    {
+        $this->checkActivation($user);
+    }
+
+    /**
+     * Security check during authorization check.
+     *
+     * @return mixed
+     */
+    public function check(UserInterface $user)
+    {
+        $this->checkActivation($user);
+    }
+
+    /**
+     * @param UserInterface $user
+     */
+    protected function checkActivation(UserInterface $user)
+    {
+        $this->activations = app(
+            'Anomaly\Streams\Addon\Module\Users\Activation\Contract\ActivationRepositoryInterface'
+        );
+
+        $activation = $this->activations->findByUserId($user->getUserId());
+
+        if (!$activation instanceof ActivationInterface or !$activation->isComplete()) {
+
+            throw new UserNotActivatedException("Your account has not been activated.");
+        }
+    }
+}
+ 
