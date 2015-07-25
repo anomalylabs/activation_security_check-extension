41098e7fc2f994f7b20b054872f6084e7aca6f1e
Ryan Thompson <service@aiwebsystems.com>
2015-07-25 17:44:25 -0500

    Lang

--- resources/lang/en/addon.php
+++ resources/lang/en/addon.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'name'        => 'Activation Security Check',
+    'title'       => 'Activation Security Check',
+    'name'        => 'Activation Security Check Extension',
     'description' => 'A security check extension that assures users are activated.'
 ];
